2c588d62811f54f702ca30fbac1d5ed45959a9c5
Francisco <francisco.dominguez.lerma@gmail.com>
2018-05-06 14:08:01 +0200

    fixed install script

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+target_path=/usr/local/bin/sps
+
 if [[ $EUID -ne 0 ]]; then
-	echo "You must be a root user"
+	echo "You must be root"
+	exit 1
+
+elif [ -f $target_path ]; then
+	echo "sps is already installed in $target_path"
 	exit 1
 else
-	cp sps.py /usr/bin/sps
-	chmod +x /usr/bin/sps
+	cp sps.py $target_path
+	chmod +x $target_path
 fi
 
-if [ -f /usr/bin/sps ]; then
-	echo "sps installed correctly in /usr/bin/sps"
+if [ -f $target_path ]; then
+	echo "sps installed correctly in $target_path"
 	exit 0
 else
 	echo "Unexpected ERROR during installation!"
